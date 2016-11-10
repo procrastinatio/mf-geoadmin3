3533c611db37068f4f62d3b11cd24f5be16f76f9
Gilbert Jeiziner <gjn@space.ch>
2016-11-10 08:59:05 +0100

    Bridge tests

--- src/components/map/MapService.js
+++ src/components/map/MapService.js
@@ -564,11 +564,11 @@ goog.require('ga_urlutils_service');
 
               // 3D Tileset
               var tileset3d = [
-                'ch.swisstopo.swisstlm3d.3d',
+                'ch.swisstopo.bridgestest.3d',
                 'ch.swisstopo.swissnames3d.3d'
               ];
               var tilesetTs = [
-                '20160909',
+                '20161111',
                 '20160909'
               ];
 
